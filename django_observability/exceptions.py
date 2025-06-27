c42265b4741fcde0296bd88ec9dc079146e97f4b
Mahdi <mahdighadiriafzal@gmail.com>
2025-06-27 17:25:37 +0330

    Create exceptions.py

---
+++ django_observability/exceptions.py
@@ -0,0 +1,8 @@
+class ObservabilityError(Exception):
+    """Base exception for Django Observability errors."""
+    pass
+
+
+class ConfigurationError(ObservabilityError):
+    """Raised when configuration is invalid."""
+    pass
